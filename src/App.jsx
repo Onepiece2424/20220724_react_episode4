2e0357987500bf0857602f991e46c00fd0603832
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-25 07:23:21 +0900

    Stateを用いてカウントアップ機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+//stateをimportする。
+import { useState } from "react";
+
 //ColordMessageをimportする。
 import { ColoredMessage } from "./components/ColoredMessage";
 
 //App関数の定義
 //exportの記述で他のファイルでも使用できるようになる。
 export const App = () => {
+  //Stateの定義
+  const [num, setNum] = useState(0);
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
 
   return (
@@ -14,6 +19,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">"元気です！"</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
