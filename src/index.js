515f58fa325b965a27919069cff5d608b6992817
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:21:43 +0900

    return以降が複数行の時の書き方

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 
 //App関数の定義
 const App = () => {
-  return <h1>こんにちは！</h1>;
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
 };
 
 //引数の設定
