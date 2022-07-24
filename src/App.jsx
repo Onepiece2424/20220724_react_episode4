88e2e8d2843e59d2d69d0748de78b948911bde01
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:29:42 +0900

    イベントの割り当て

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 //App関数の定義
 //exportの記述で他のファイルでも使用できるようになる。
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <>
       <h1>こんにちは！</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
