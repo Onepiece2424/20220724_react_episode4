4e62dea966ff9e71eac0a413d381a10824aa0acb
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:46:34 +0900

    props作成のための準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+//ColordMessageをimportする。
+import { ColoredMessage } from "./components/ColoredMessage";
+
 //App関数の定義
 //exportの記述で他のファイルでも使用できるようになる。
 export const App = () => {
@@ -5,14 +8,16 @@ export const App = () => {
     alert();
   };
 
-  const constStyle = {
-    color: "blue",
+  const contentPinkStyle = {
+    color: "pink",
     fontSize: "20px"
   };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={constStyle}>お元気ですか？</p>
+      <p style={contentPinkStyle}>元気です！</p>
+      <ColoredMessage />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
---
+++ src/components/ColoredMessage.jsx
@@ -0,0 +1,8 @@
+export const ColoredMessage = () => {
+  const constStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
+
+  return <p style={constStyle}>お元気ですか？</p>;
+};
