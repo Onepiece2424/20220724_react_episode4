d964f84c064c6437ed857837fe40cd93753c2810
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:25:32 +0900

    App 関数をApp.jsへexportし、index.jsにimportする。

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 //ReactDOMモジュールのインポート
 import ReactDOM from "react-dom";
-
-//App関数の定義
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+//App関数を使用する。
+import { App } from "./App";
 
 //引数の設定
 ReactDOM.render(<App />, document.getElementById("root"));
