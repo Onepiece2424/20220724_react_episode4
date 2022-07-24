ba54592a85bff281e85b73a006da4404aba612c2
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:15:05 +0900

    App関数の設定と引数の設定

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+//ReactDOMモジュールのインポート
 import ReactDOM from "react-dom";
+
+//App関数の定義
+const App = () => {
+  return null;
+};
+
+//引数の設定
+ReactDOM.render(<App />, document.getElementById("root"));
