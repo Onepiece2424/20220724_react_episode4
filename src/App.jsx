a55e47faeeb8d0395f93136b336a55fae2032fb6
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-24 19:33:45 +0900

    スタイルを変数に置き換えて表示

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ export const App = () => {
   const onClickButton = () => {
     alert();
   };
+
+  const constStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <p style={constStyle}>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
